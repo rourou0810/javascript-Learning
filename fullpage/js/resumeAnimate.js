fbd94536af50980ac8bbf4939f7abb084e87ab44
rourou0810 <awei_0810@163.com>
2017-09-18 17:54:56 +0800

    add meilu

---
+++ fullpage/js/resumeAnimate.js
@@ -0,0 +1,41 @@
+  $(function(){
+
+    
+
+    function section(){
+        
+        $('.section1').delay(50).animate({
+            opacity: '1'
+        }, 100, function(){
+            $(".resume").addClass("on");
+            setTimeout(function(){
+                $(".resume").addClass("dong") 
+            },250)
+        });
+    }
+    section();
+
+    $('#dowebok').fullpage({
+        sectionsColor: ['transparent', 'transparent', 'transparent', 'transparent','transparent', 'transparent'],
+        //sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90'],
+        //paddingTop: '114px',
+        anchors:['home', 'abouts', 'services','news','clients','contacts'],
+        menu: '#menuList',
+        scrollBar:true,
+        afterRender: function(anchorLink, index){
+            wow = new WOW({
+                  animateClass: 'animated',
+              });
+             wow.init();
+        }
+    });
+
+    $(".news-item").hover(
+        function () {
+            $(this).find('.common-btn-white').addClass("common-btn-yellow");
+        },
+        function () {
+            $(this).find('.common-btn-white').removeClass("common-btn-yellow");
+        }
+    );
+});
